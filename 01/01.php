b3f02da56eac97f5403b1bc82f8ee876f567946a
Laurens Hoek <lrhoek@gmail.com>
2023-12-01 09:10:29 +0100

    cleanup

--- 01/01.php
+++ 01/01.php
@@ -1,27 +1,21 @@
 <?php
 
-$input = file_get_contents('input');
+function calibrations(string $input) : array {
+    $lines = explode(PHP_EOL, $input);
+    $digitLines = filter_var_array($lines, FILTER_SANITIZE_NUMBER_INT);
+    return array_map(fn($digits) => (int)(substr($digits, 0, 1) . substr($digits, -1, 1)), $digitLines);
+}
 
-$lines = explode(PHP_EOL, $input);
-
-$digitLines = filter_var_array($lines, FILTER_SANITIZE_NUMBER_INT);
-
-$calibrations = array_map(fn ($digits) => (int) (substr($digits, 0, 1) . substr($digits, -1, 1)), $digitLines);
-
-echo array_sum($calibrations).PHP_EOL;
-
-function replace($string, $index) {
+function replace(string $string, int $index) : string {
     $current = substr($string, $index, 5);
     $current = str_replace(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], range(1, 9), $current);
     return substr_replace($string, $current, $index, strlen($current));
 }
 
+$input = file_get_contents('input');
+
+echo array_sum(calibrations($input)).PHP_EOL;
+
 $input = array_reduce(range(0, strlen($input)), replace(...), $input);
 
-$lines = explode(PHP_EOL, $input);
-
-$digitLines = filter_var_array($lines, FILTER_SANITIZE_NUMBER_INT);
-
-$calibrations = array_map(fn ($digits) => (int) (substr($digits, 0, 1) . substr($digits, -1, 1)), $digitLines);
-
-echo array_sum($calibrations).PHP_EOL;
+echo array_sum(calibrations($input)).PHP_EOL;
